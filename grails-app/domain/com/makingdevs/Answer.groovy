ef101b6e136922ef3c62e13f2badb9dc70f830ee
Jorge Acosta <jorge@makingdevs.com>
2014-01-10 17:29:07 -0600

    coding #5 Se agrego la relacion con question

--- grails-app/domain/com/makingdevs/Answer.groovy
+++ grails-app/domain/com/makingdevs/Answer.groovy
@@ -4,7 +4,9 @@ class Answer {
 
 	String description
 
-    static constraints = {
-    	description blank:false, size:1..1000
-    }
+  static constraints = {
+   description blank:false, size:1..1000
+ }
+
+ static belongsTo = [question: Question]
 }
