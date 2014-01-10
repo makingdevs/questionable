5f64cc6f7f4ecb11bf3899d290297ff08536f81e
Jorge Acosta <jorge@makingdevs.com>
2014-01-10 10:22:17 -0600

    coding #3 agregando accion de controller y ToString a clasedominio

--- grails-app/controllers/com/makingdevs/QuestionController.groovy
+++ grails-app/controllers/com/makingdevs/QuestionController.groovy
@@ -2,5 +2,19 @@ package com.makingdevs
 
 class QuestionController {
 
-    def index() { }
+  def index() { }
+
+  def create(){
+    def pregunta = new Question(params)
+    if(pregunta.save(flush:true)){
+      redirect action:"show", id:pregunta.id
+    }else{
+      render view:"create"
+    }
+  }
+
+  def show(){
+    def question = Question.get(params.id)
+    [question:question]
+  }
 }
--- grails-app/domain/com/makingdevs/Question.groovy
+++ grails-app/domain/com/makingdevs/Question.groovy
@@ -1,13 +1,14 @@
 package com.makingdevs
 
+@groovy.transform.ToString
 class Question {
 
 	String description
 	QuestionType questionType 
 
-    static constraints = {
-    	description blank:false, size:1..1000
-    }
+	static constraints = {
+		description blank:false, size:1..1000
+	}
 
-    static hasMany = [answers : Answer]
+	static hasMany = [answers : Answer]
 }
