6518a8f5ae75b54a5577161fa51764d0abd3511f
Jorge Acosta <jorge@makingdevs.com>
2014-01-13 12:00:35 -0600

    coding #5 se afino detalles sobre la clase

--- grails-app/domain/com/makingdevs/Questionary.groovy
+++ grails-app/domain/com/makingdevs/Questionary.groovy
@@ -3,13 +3,12 @@ package com.makingdevs
 @groovy.transform.ToString
 class Questionary {
 
-  String titule
+  String title
   String description
-  QuestionaryCategory questionaryCategory
 
   static constraints = {
-    titule blank:false, size:1..30
-    description blank:false, size:1..1000
+    title blank:false, size:1..30
+    description blank:false, size:1..100
   }
 
   static hasMany = [questions : Question]
