84930187edbe83bf8d7e43919c5c105f4f2981ab
Jorge Acosta <jorge@makingdevs.com>
2014-01-13 15:16:25 -0600

    coding #6 se agrego librerias de jquery para trabajar con remote

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -40,6 +40,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        runtime ":jquery:1.10.2.2"
         build(":release:3.0.1",
               ":rest-client-builder:1.0.3") {
             export = false
