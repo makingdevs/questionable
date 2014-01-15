69a3cdc229d85a12b7c77a3395510e73890af86c
Jorge Acosta <jorge@makingdevs.com>
2014-01-15 09:25:56 -0600

    coding #12 ajustando la prueba de unidad

--- test/unit/com/makingdevs/QuestionServiceSpec.groovy
+++ test/unit/com/makingdevs/QuestionServiceSpec.groovy
@@ -6,6 +6,7 @@ import spock.lang.Specification
 
 
 @TestFor(QuestionService)
+@Mock([Question])
 class QuestionServiceSpec extends Specification{
 
   def "Evaluar una pregunta abierta"(){
@@ -38,32 +39,7 @@ class QuestionServiceSpec extends Specification{
   }
 
   private Question anOpenQuestion(){
-
+    Question(description:"¿Es esta una pregunta abierta?, Describa",questionType:QuestionType.OPEN).save()
   }
-
-  def "Evaluar una pregunta de opcion multiple"(){
-    given:"Dada una pregunta de opcion multiple"
-    def pregunta = new Question()
-    pregunta.description="Tipo de lenguaje groovy"
-    pregunta.questionType="MULTIPLE_CHOICE"
-    and:"y dada su respuestas de la pregunta"
-    def answer1 = new Answer()
-    answer1.description="funcional"
-    answer1.solution="false"
-    def answer2 = new Answer()
-    answer2,description="dinamico"
-    answer2.solution="true"
-    def answer3 = new Answer()
-    answer3.description="estatico"
-    answer3.solution="false"
-    pregunta.addToAnswers(answer1)
-    pregunta.addToAnswers(answer2)
-    pregunta.addToAnswers(answer3)
-    when:"El usuario responde la pregunta correctamente"
-    def respuestaUsuario = "dinamico"
-    def evaluacion = service.evaluar(respuestaUsuario,pregunta)
-    then:"Evaluando la pregunta el usuario debe de obtiene 1 punto"
-    assert evaluacion == 1
-  } 
 
 }
