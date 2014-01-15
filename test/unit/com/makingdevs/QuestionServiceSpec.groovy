d3fb88e8e5848d130cbb6a0dd20f733322e1ae91
Jorge Acosta <jorge@makingdevs.com>
2014-01-15 12:15:39 -0600

    coding #12 agregando prueba para falso y cierto

--- test/unit/com/makingdevs/QuestionServiceSpec.groovy
+++ test/unit/com/makingdevs/QuestionServiceSpec.groovy
@@ -6,22 +6,36 @@ import spock.lang.Specification
 
 
 @TestFor(QuestionService)
-@Mock([Question])
+@Mock([Question, Answer])
 class QuestionServiceSpec extends Specification{
 
   def "Evaluar una pregunta abierta"(){
     given:
       def question = anOpenQuestion()
     when:
-      def answer = "Esta es una respuesta abierta a la pregunta"
-      def evaluate = service.evaluateAnswer(question.id,answer)
+      def evaluate = service.evaluateAnswer(question.id,_answer)
     then:
-      evaluate == 1.0
+      evaluate == rating
       question.questionType == QuestionType.OPEN
+    where:
+      _answer                                       || rating
+      "Esta es una respuesta abierta a la pregunta" || 1.0
+      ""                                            || 0.0
   }
 
   def "Evaluar una pregunta con opciones de falso o verdadero"(){
-    
+    given:
+      def question = aTrueFalseQuestion()
+    when:
+      def answer = _answer
+      def evaluate = service.evaluateAnswer(question.id, answer)
+    then:
+      evaluate == rating
+      question.questionType == QuestionType.TRUE_FALSE
+    where: 
+      _answer || rating
+      true    || 0.0
+      false   || 1.0
   }
 
   def "Evaluar una pregunta con multiples opciones siendo una la correcta"(){
@@ -34,6 +48,14 @@ class QuestionServiceSpec extends Specification{
 
   private Question anOpenQuestion(){
     new Question(description:"¿Es esta una pregunta abierta?, Describa",questionType:QuestionType.OPEN).save()
+  }
+
+  private Question aTrueFalseQuestion(){
+    def question = new Question(description:"¿Falso es igual a verdadero?", questionType:QuestionType.TRUE_FALSE)
+    def answer = new Answer(solution:false)
+    question.addToAnswers(answer)
+    question.save(validate:false)
+    question
   }
 
 }
