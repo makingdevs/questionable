111ea8673e2ce621e94aa505277d12697667bf6d
Jorge Acosta <jorge@makingdevs.com>
2014-01-15 12:14:39 -0600

    coding #12 agregando funcionalidad

--- grails-app/services/com/makingdevs/QuestionService.groovy
+++ grails-app/services/com/makingdevs/QuestionService.groovy
@@ -8,4 +8,23 @@ class QuestionService {
     def serviceMethod() {
 
     }
+
+    def evaluateAnswer(question_id,answer){
+      def question = Question.get(question_id) 
+      switch(question.questionType) {
+        case QuestionType.OPEN:
+          if (answer)
+            1.0
+          else
+            0.0
+        break
+        case QuestionType.TRUE_FALSE:
+          def Boolean answerQuestion=question.answers.solution
+          if (answer == answerQuestion)
+           0.0
+          else
+           1.0
+        break
+      }
+    }
 }
