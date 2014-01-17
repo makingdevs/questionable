bb70c914cc2a38c3fa17431ac6831d1e91bdcab9
Jorge Acosta <jorge@makingdevs.com>
2014-01-17 12:35:26 -0600

    coding #17 refactor de asignacion de avriable lñocal

--- grails-app/services/com/makingdevs/QuestionService.groovy
+++ grails-app/services/com/makingdevs/QuestionService.groovy
@@ -5,58 +5,41 @@ import grails.transaction.Transactional
 @Transactional
 class QuestionService {
 
-    def serviceMethod() {
+  def evaluateAnswer(question_id,answer){
+    def ratings = 0
+    def question = Question.get(question_id) 
+    switch(question.questionType) {
 
-    }
+      case QuestionType.OPEN:
+      if (answer)
+      ratings = 1.0
+      break
 
-    def evaluateAnswer(question_id,answer){
-      def question = Question.get(question_id) 
-      switch(question.questionType) {
+      case QuestionType.TRUE_FALSE:
+      if(question.answers.first().solution==answer)
+      ratings = 1.0
+      break
 
-        case QuestionType.OPEN:
-          if (answer)
-            return 1.0
-          else
-            return 0.0
-        break
-
-        case QuestionType.TRUE_FALSE:
-          if(question.answers.first().solution==answer)
-            1.0
-          else
-            0.0
-        break
-
-        case QuestionType.MULTIPLE_CHOICE:
-        def answer_user = Answer.get(answer)
-        def id_answer_solution
-        int i = 0
-        while(question.answers.getAt(i).solution==true) {
-          id_answer_solution = question.answers.getAt(i)
-          i++
-        }
-        if (answer_user.id==id_answer_solution.id){
-          return 1.0
-        }else{
-          return 0.0          
-        }
-        break
-
-        case QuestionType.MULTIPLE_RESPONSE:
-        def answers_user = Answer.findAllByIdInList(answer)
-        def evaluate
-
-        def answerSolutionTrue = question.answers.findAll { it.solution }
-        def acertadasEnTrue = (answerSolutionTrue.id.intersect(answers_user.id)).size()
-        def answerSolutionFalse = question.answers.findAll { !it.solution }
-        def acertadasEnfalse = (answerSolutionFalse.id.intersect(answers_user.id)).size()
-
-        def point = Math.round(( 1 / question.answers.size() ) * 100) / 100
-        (acertadasEnTrue + acertadasEnfalse) * point
-
-        break
-
-        
+      case QuestionType.MULTIPLE_CHOICE:
+      def answer_user = Answer.get(answer)
+      def id_answer_solution
+      int i = 0
+      while(question.answers.getAt(i).solution==true) {
+        id_answer_solution = question.answers.getAt(i)
+        i++
       }
+      if (answer_user.id==id_answer_solution.id)
+        ratings = 1.0
+
+      break
+
+      case QuestionType.MULTIPLE_RESPONSE:
+
+
+      return 0
+
+      break
     }
+    ratings
+  }
 }
