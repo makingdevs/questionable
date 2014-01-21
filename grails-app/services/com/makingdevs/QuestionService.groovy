f1d6ca6668816280a52a1c798325477723262344
Jorge Acosta <jorge@makingdevs.com>
2014-01-20 23:12:33 -0600

    coding #18 refacor de metodo y solucion para bug de evaluate for multiple choice

--- grails-app/services/com/makingdevs/QuestionService.groovy
+++ grails-app/services/com/makingdevs/QuestionService.groovy
@@ -22,17 +22,10 @@ class QuestionService {
 
       case QuestionType.MULTIPLE_CHOICE:
       def answer_user = Answer.get(answer)
-      def id_answer_solution
-      int i = 0
-      while(question.answers.getAt(i).solution==true) {
-        id_answer_solution = question.answers.getAt(i)
-        i++
-      }
-      if (answer_user.id==id_answer_solution.id) ratings = 1.0
+      if (answer_user.id in question.answers.id&&answer_user.solution==true) ratings=1.0 else ratings=0.0
       break
 
       case QuestionType.MULTIPLE_RESPONSE:
-
       def answersFromUser = Answer.findAllByIdInList(answer)
       def checks = 0
       question.answers.each { answerQuestion ->
