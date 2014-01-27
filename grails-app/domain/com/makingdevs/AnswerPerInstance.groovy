25a16da2b5d1600c24e83a4eb1578321a2b8a9db
Jorge Acosta <jorge@makingdevs.com>
2014-01-26 21:44:09 -0600

    coding #23 complementando la especificacion de la clase

--- grails-app/domain/com/makingdevs/AnswerPerInstance.groovy
+++ grails-app/domain/com/makingdevs/AnswerPerInstance.groovy
@@ -5,6 +5,13 @@ class AnswerPerInstance {
   Date dateCreated
   Date lastUpdated
 
+  Question question
+  OpenAnswerPerUser openAnswerPerUser
+
+  static belongsTo = [questionaryPerInstance:QuestionaryPerInstance]
+
+  static hasMany = [answerPerUser:AnswerPerUser]
+
   static constraints = {
  }
 }
