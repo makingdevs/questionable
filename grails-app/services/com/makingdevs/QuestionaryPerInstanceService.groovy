1e837d5774b56e755d6d3c58ae42092de66c47c2
Jorge Acosta <jorge@makingdevs.com>
2014-01-31 13:36:14 -0600

    coding #28 en procceso

--- grails-app/services/com/makingdevs/QuestionaryPerInstanceService.groovy
+++ grails-app/services/com/makingdevs/QuestionaryPerInstanceService.groovy
@@ -11,8 +11,19 @@ class QuestionaryPerInstanceService {
     questionary.questions.each{it->
       questionaryPerInstance.addToAnswerPerInstances(new AnswerPerInstance(question:it))
     }
-    questionaryPerInstance.save(true:false)
+    questionaryPerInstance.save(flush:false)
     questionaryPerInstance
   }
 
+  def addAnswer(idPregunta,respuesta,idQuestionaryPerInstance){
+    def question=Question.get(idPregunta)
+    def questionaryPerInstance=QuestionaryPerInstance.get(idQuestionaryPerInstance)
+    questionaryPerInstance.answerPerInstances.each{it-> 
+      if(it.question.id==question.id){
+      it.addToOpenAnswerPerUsers(new OpenAnswerPerUser(userAnswer:respuesta)).save()
+      }
+    }
+    questionaryPerInstance.save(flush:true)
+    questionaryPerInstance
+  }
 }
