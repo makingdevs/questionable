ce95208d7fbdf2010916288af93d668e4cae8995
Jorge Acosta <jorge@makingdevs.com>
2014-02-05 13:04:32 -0600

    coding #34 implementando metodo

--- grails-app/services/com/makingdevs/QuestionaryPerInstanceLinkService.groovy
+++ grails-app/services/com/makingdevs/QuestionaryPerInstanceLinkService.groovy
@@ -1,10 +1,18 @@
 package com.makingdevs
 
+import com.makingdevs.*
 import grails.transaction.Transactional
 
 class QuestionaryPerInstanceLinkService {
 
-  def createQuestionaryPerInstance(def instance, Long questionaryId) {
+  def questionaryPerInstanceService
 
+  def createQuestionaryPerInstance(def instance, Long questionaryId) {
+    def questionaryPerInstanceLink =new QuestionaryPerInstanceLink(
+      questionaryPerInstance:questionaryPerInstanceService.instanceQuestionary(questionaryId),
+      type:instance.class.getSimpleName(),
+      questionaryPerInstanceRef:instance.id)
+      .save(flush:true)
+    questionaryPerInstanceLink
   }
 }
