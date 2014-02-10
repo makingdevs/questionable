44c463f914ccc368fd5eb64f9d98dd6ba14f0735
Jorge Acosta <jorge@makingdevs.com>
2014-02-10 14:03:59 -0600

    coding #41 se refactorizo taglib

--- grails-app/taglib/com/makingdevs/QuestionaryTagLib.groovy
+++ grails-app/taglib/com/makingdevs/QuestionaryTagLib.groovy
@@ -4,15 +4,9 @@ class QuestionaryTagLib {
     
   def showQuestionaryForThisInstance = { attrs, body ->
     def listQuestioanryAvailable=Questionary.list()
-    out << "<ul>"
-      listQuestioanryAvailable?.each { questionary ->
-        out << """
-            <li>${g.link(controller:'questionary',action:'answerQuestionary',id:"${questionary.id}"){"${questionary.codeName}"}}
-            <a class='btn-small btn-primary' href=''><i class='icon-plus-sign'></i>Hola</a>
-            <a class='btn-small btn-primary' href=''><i class='icon-plus-sign'></i>Hola</a>
-            </li>
-        """
-      }
-    out << "</ul>"
+    out << render(template:"/questionaryPerInstance/questionaryForThisInstance", 
+      model:[listQuestioanryAvailable:listQuestioanryAvailable,
+            idInstance:attrs.instance.id,
+            typeInstance:attrs.instance.class.getSimpleName()])
   }
 }
