2353a887abac3a74bff635787352f100ffce948a
Jorge Acosta <jorge@makingdevs.com>
2014-02-10 12:56:06 -0600

    coding #41 en procceso

--- grails-app/taglib/com/makingdevs/QuestionaryTagLib.groovy
+++ grails-app/taglib/com/makingdevs/QuestionaryTagLib.groovy
@@ -5,9 +5,12 @@ class QuestionaryTagLib {
   def showQuestionaryForThisInstance = { attrs, body ->
     def listQuestioanryAvailable=Questionary.list()
     out << "<ul>"
-      listQuestioanryAvailable.each { questionary ->
-        out << """\
-          <li>${g.link(controller:'questionary',action:'answerQuestionary',id:"${questionary.id}"){"${questionary.codeName}"}}</li>
+      listQuestioanryAvailable?.each { questionary ->
+        out << """
+            <li>${g.link(controller:'questionary',action:'answerQuestionary',id:"${questionary.id}"){"${questionary.codeName}"}}
+            <a class='btn-small btn-primary' href=''><i class='icon-plus-sign'></i>Hola</a>
+            <a class='btn-small btn-primary' href=''><i class='icon-plus-sign'></i>Hola</a>
+            </li>
         """
       }
     out << "</ul>"
