641428f1b0e2ce1148791240691941756cd48a14
Jorge Acosta <jorge@makingdevs.com>
2014-02-12 12:47:43 -0600

    coding #47 se corrigio contraints para permitir respuestas en blanco

--- grails-app/domain/com/makingdevs/OpenAnswerPerUser.groovy
+++ grails-app/domain/com/makingdevs/OpenAnswerPerUser.groovy
@@ -10,6 +10,6 @@ class OpenAnswerPerUser {
   static belongsTo = [answerPerInstance:AnswerPerInstance]
 
   static constraints = {
-    userAnswer size:1..1000, blank:false
+    userAnswer size:0..1000, blank:false, nullable: true
   }
 }
