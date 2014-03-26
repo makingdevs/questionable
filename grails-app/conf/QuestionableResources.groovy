a34cd967da283923f2b379c73da4b1986fe9252a
Jorge Acosta <jorge@makingdevs.com>
2014-03-26 12:05:05 -0600

    coding #59 se cambio forma de declarar los resources

--- grails-app/conf/QuestionableResources.groovy
+++ grails-app/conf/QuestionableResources.groovy
@@ -6,7 +6,6 @@ modules = {
   }
   
   questionable{
-    dependsOn 'syntaxhighlighter'
   	resource url:'css/bootstrap-responsive.min.css'
   	resource url:'css/bootstrap.min.css'
   	resource url:'images/glyphicons-halflings-white.png'
@@ -15,19 +14,19 @@ modules = {
   }
 
   syntaxhighlighter{
-    resource id:"shCore", url:[plugin:'questionable',dir:'js/syntaxhighlighter',file:'shCore.js'], disposition:'head'
-    resource id:"shCore",url:[plugin:"questionable",dir:"js/syntaxhighlighter",file:"shCore.css"], disposition:'head'
-    resource id:"shThemeDefault",url:[plugin:"questionable",dir:"js/syntaxhighlighter",file:"shThemeDefault.css"], disposition:'head'
-    resource id:"configSyntax",url:[plugin:"questionable",dir:"js/syntaxhighlighter",file:"configSyntax.js"], disposition:'head'
-    resource id:"shBrushJScript",url:[plugin:"questionable",dir:"js/syntaxhighlighter/archivosJS",file:"shBrushJScript.js"], disposition:'head'
-    resource id:"shBrushCss",url:[plugin:"questionable",dir:"js/syntaxhighlighter/archivosJS",file:"shBrushCss.js"], disposition:'head'
-    resource id:"shBrushGroovy",url:[plugin:"questionable",dir:"js/syntaxhighlighter/archivosJS",file:"shBrushGroovy.js"], disposition:'head'
-    resource id:"shBrushJava",url:[plugin:"questionable",dir:"js/syntaxhighlighter/archivosJS",file:"shBrushJava.js"], disposition:'head'
-    resource id:"shBrushRuby",url:[plugin:"questionable",dir:"js/syntaxhighlighter/archivosJS",file:"shBrushRuby.js"], disposition:'head'
-    resource id:"shBrushPython",url:[plugin:"questionable",dir:"js/syntaxhighlighter/archivosJS",file:"shBrushPython.js"], disposition:'head'
-    resource id:"shBrushSql",url:[plugin:"questionable",dir:"js/syntaxhighlighter/archivosJS",file:"shBrushSql.js"], disposition:'head'
-    resource id:"shBrushXml",url:[plugin:"questionable",dir:"js/syntaxhighlighter/archivosJS",file:"shBrushXml.js"], disposition:'head'
-    resource id:"shBrushScala",url:[plugin:"questionable",dir:"js/syntaxhighlighter/archivosJS",file:"shBrushScala.js"], disposition:'head'
+    resource url:'js/syntaxhighlighter/shCore.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/shCore.css', disposition: 'head'
+    resource url:'js/syntaxhighlighter/shThemeMidnight.css', disposition: 'head'
+    resource url:'js/syntaxhighlighter/configSyntax.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/archivosJS/shBrushJScript.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/archivosJS/shBrushCss.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/archivosJS/shBrushGroovy.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/archivosJS/shBrushJava.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/archivosJS/shBrushRuby.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/archivosJS/shBrushPython.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/archivosJS/shBrushSql.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/archivosJS/shBrushXml.js', disposition: 'head'
+    resource url:'js/syntaxhighlighter/archivosJS/shBrushScala.js', disposition: 'head'
   }
 
 }
