e8a15932182a9f194b4823833e06edf98750a08b
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-06-23 10:50:06 -0500

    coding #74 corrigiendo indentacion de AnswerService

--- test/unit/com/makingdevs/AnswerServiceSpec.groovy
+++ test/unit/com/makingdevs/AnswerServiceSpec.groovy
@@ -5,32 +5,33 @@ import grails.test.mixin.support.GrailsUnitTestMixin
 import org.junit.*
 import spock.lang.*
 
+
 @TestFor(AnswerService)
 @Mock([Question,Answer])
 class AnswerServiceSpec extends Specification {
-
-	@Unroll
-	def """ Build answer from text plain "#_simpleText" """() {
-		given:
-		def simpleText = _simpleText
-		when:
-		def answer = service.buildAnswerFromText(simpleText)
-		then:      
-		answer.description == _description 
-		answer.solution == _solution
-		!answer.id
-		where:
-		_simpleText                             || _description                       | _solution
-		"(*) un fw"			                        ||  "un fw"                           | true
-		"() un lenguaje"                        ||  "un lenguaje"                     | false
-		"( ) una herramienta"                   ||  "una herramienta"                 | false
-		"[*] un lenguaje dinamico"              ||  "un lenguaje dinamico"            | true
-		"[] una herramiemnta del so"            ||  "una herramiemnta del so"         | false
-		"[*] un lenguaje orientado a objetos"   ||  "un lenguaje orientado a objetos" | true
-		"F"                                     ||  "Falso"                           | false
-		"V*"                                    ||  "Verdadero"                       | true
-		"F*"                                    ||  "Falso"                           | true
-		"V"                                     ||  "Verdadero"                       | false
-	}
-
+  
+  @Unroll
+  def """ Build answer from text plain "#_simpleText" """(){
+    given:
+      def simpleText =  _simpleText
+    when:
+      def answers = service.buildAnswerFromText(simpleText)
+    then:
+      answer.description == _description
+      answer.solution == _solution
+      !answer.id
+    where:
+      _simpleText                             ||  _description                      | _solution
+      "(*) un fw"                             ||  "un fw"                           | true
+      "() un lenguaje"                        ||  "un lenguaje"                     | false
+      "( ) una herramienta"                   ||  "una herramienta"                 | false
+      "[*] un lenguaje dinamico"              ||  "un lenguaje dinamico"            | true
+      "[] una herramiemnta del so"            ||  "una herramiemnta del so"         | false
+      "[*] un lenguaje orientado a objetos"   ||  "un lenguaje orientado a objetos" | true
+      "F"                                     ||  "Falso"                           | false
+      "V*"                                    ||  "Verdadero"                       | true
+      "F*"                                    ||  "Falso"                           | true
+      "V"                                     ||  "Verdadero"                       | false
+  }
+  
 }
