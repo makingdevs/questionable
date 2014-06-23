d288105a3de61cab7ddf56b5ab23f8191b5fe7c8
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-06-23 11:13:42 -0500

    coding #75 corrigiendo indentacion de TagService

--- test/unit/com/makingdevs/TagsServiceSpec.groovy
+++ test/unit/com/makingdevs/TagsServiceSpec.groovy
@@ -4,43 +4,44 @@ import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import spock.lang.*
 
+
 @TestFor(TagsService)
 @Mock([Question])
 class TagsServiceSpec extends Specification {
 
-	@Unroll
-	def "addTagsToQuestionFromSimpleText"(){
-		given:
-			def tags = "groovy,grails,spring"
-			def currentTags = []
-			def question = new Question().save(validate:false)
- 		and:
-			Question.metaClass.parseTags = {tagsToSave ->currentTags = tagsToSave.split(",")} 			
-			Question.metaClass.getTags = { currentTags }
-		when: 
-			service.addTagsToAQuestionFromSimpleText(question,tags)
-		then:			
-			question.tags == tags.tokenize(',')
-	}
+  @Unroll
+  def "addTagsToQuestionFromSimpleText"(){
+    given:
+      def tags = "groovy,grails,spring"
+      def currentTags = []
+      def question = new Question().save(validate:false)
+    and:
+      Question.metaClass.parseTags = {tagsToSave ->currentTags = tagsToSave.split(",")}
+      Question.metaClass.getTags = { currentTags }
+    when:
+      service.addTagsToAQuestionFromSimpleText(question,tags)
+    then:
+      question.tags == tags.tokenize(',')
+  }
 
-	@Unroll
-	def "updateTagsToQuestionFromSimpleText"(){
-		given:			
-			def tags = _tags
-			def currentTags = _currentTags
-			def question = new Question().save(validate:false)
-		and:
-			Question.metaClass.parseTags = {tagsToSave -> currentTags = tagsToSave.split(",")}
-			Question.metaClass.setTags = {listOfTags -> currentTags = listOfTags}
-			Question.metaClass.getTags = { currentTags }
-		when:
-			service.updateTagsToAQuestionFromSimpleText(question,tags)
-		then:
-			question.tags == currentTags
-		where:
-			_tags						|| _currentTags
-			''							||	[]
-			'groovy,grails'	||	['groovy','grails']
-	}
-	
+  @Unroll
+  def "updateTagsToQuestionFromSimpleText"(){
+    given:
+      def tags = _tags
+      def currentTags = _currentTags
+      def question = new Question().save(validate:false)
+    and:
+      Question.metaClass.parseTags = {tagsToSave -> currentTags = tagsToSave.split(",")}
+      Question.metaClass.setTags = {listOfTags -> currentTags = listOfTags}
+      Question.metaClass.getTags = { currentTags }
+    when:
+      service.updateTagsToAQuestionFromSimpleText(question,tags)
+    then:
+      question.tags == currentTags
+    where:
+      _tags           ||  _currentTags
+      ''              ||  []
+      'groovy,grails' ||  ['groovy','grails']
+  }
+
 }
