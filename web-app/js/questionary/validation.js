5da88a39c7823ad99fc0bc887106c5e2c8b1e258
temocpalma <temopalmag@gmail.com>
2016-06-20 12:49:41 -0500

    Se crea estructura para update a grails 3

--- web-app/js/questionary/validation.js
+++
@@ -1,8 +0,0 @@
-   $("#questionaryForm").validate({
-      errorPlacement: function(error, element) {
-        error.addClass("help-inline"));
-      },
-      submitHandler: function(form) {
-        form.submit();
-      }
-    });
--- web-app/js/syntaxhighlighter/configSyntax.js
+++
@@ -1,4 +0,0 @@
-
-SyntaxHighlighter.config.bloggerMode = true;
-SyntaxHighlighter.config.clipboardSwf = 'http://alexgorbatchev.com/pub/sh/current/scripts/clipboard.swf';
-SyntaxHighlighter.all();
