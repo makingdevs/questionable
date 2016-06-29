653f0c8dd5d7d98c829212cfa5acc0c9293cc419
temocpalma <temopalmag@gmail.com>
2016-06-29 15:59:53 -0500

    #80 Agregando printlns para revisión de tests

--- grails-app/services/com/makingdevs/QuestionAndAnswerService.groovy
+++ grails-app/services/com/makingdevs/QuestionAndAnswerService.groovy
@@ -9,57 +9,62 @@ class QuestionAndAnswerService {
   def questionService
   def answerService
 
-  def createQuestionsWithAnswersFromSimpleText(fullQuestions){            
+  def createQuestionsWithAnswersFromSimpleText(fullQuestions){
     def questions = []
     def questionTags = []
     def answers = []
     def lines = fullQuestions.split('\n|\t')
     lines = lines*.trim()
-      
+
     for(def i=0;i<lines.size();i++){
       if(lines[i]==~ /\#.+/){
         while(lines[i+1] != null && !(isThisLineAnswer(lines[i+1]) || isThisLineAQuestion(lines[i+1]))){
           if(isThisLineTheBeginningOfTheCode(lines[i+1])){
-            while(lines[i+1] != null && !isThisLineTheEndOfTheCode(lines[i+1])){             
+            while(lines[i+1] != null && !isThisLineTheEndOfTheCode(lines[i+1])){
               lines[i]+="\n"+lines[i+1]
               lines.remove(i+1)
             }
           }
-          if(lines[i+1] != null){            
+          if(lines[i+1] != null){
             lines[i]+="\n"+lines[i+1]
-            lines.remove(i+1)      
+            lines.remove(i+1)
           }
           else
-            throw new Exception("Cannot parse the answer, close the <pre> tag")          
+            throw new Exception("Cannot parse the answer, close the <pre> tag")
         }
       }
     }
-    
+
     lines.each{ line ->
       if(isThisLineAQuestion(line)){
+        println "IsQuestion"
         if(answers && questions){
           answers.each{ answer -> questions.last().addToAnswers(answer) }
           answers.clear()
         }
         def question = questionService.buildQuestionFromText(line.trim())
+        println "Question: ${question}"
         questions << question
         questionTags << questionService.getTagsFromText(line)
       }
-      if(line.trim() && !isThisLineAQuestion(line))
+      if(line.trim() && !isThisLineAQuestion(line)) {
         answers << answerService.buildAnswerFromText(line.trim())
-    }    
-    
-    if(questions){      
+        println "Answers: ${answers}"
+      }
+    }
+
+    if(questions){
       answers.each { answer -> questions.last().addToAnswers(answer)}
     }
-    else{      
+    else{
       throw new RuntimeException("Cannot parse answers without questions")
     }
 
+    println "Questions size: ${questions.size()}"
     questions*.save()
-    
-    questions.size().times{ i -> questions[i].setTags(questionTags[i])} 
-    
+
+    questions.size().times{ i -> questions[i].setTags(questionTags[i] ?: "")}
+
     questions
   }
 
@@ -73,9 +78,9 @@ class QuestionAndAnswerService {
 
   def isThisLineTheBeginningOfTheCode(line){
     line ==~ /\<pre*\>.*/
-  }   
+  }
 
   def isThisLineTheEndOfTheCode(line){
     line ==~ /.*\<\/pre\>.*/
-  }  
+  }
 }
