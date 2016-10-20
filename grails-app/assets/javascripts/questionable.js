3ba3d4f16db561235ac2c025ff6ac7b308f4d505
neodevelop <juan@makingdevs.com>
2016-10-20 17:16:07 -0500

    #81 usando bibliotecas syntax

---
+++ grails-app/assets/javascripts/questionable.js
@@ -0,0 +1,18 @@
+//= require third-party/jquery/dist/jquery
+//= require third-party/bootstrap/dist/js/bootstrap
+//= require third-party/syntaxhighlighter/scripts/shCore
+//= require third-party/syntaxhighlighter/scripts/shAutoloader
+//= require third-party/syntaxhighlighter/scripts/shLegacy
+
+//= require third-party/syntaxhighlighter/scripts/shBrushJScript
+//= require third-party/syntaxhighlighter/scripts/shBrushCss
+//= require third-party/syntaxhighlighter/scripts/shBrushGroovy
+//= require third-party/syntaxhighlighter/scripts/shBrushJava
+//= require third-party/syntaxhighlighter/scripts/shBrushRuby
+//= require third-party/syntaxhighlighter/scripts/shBrushPython
+//= require third-party/syntaxhighlighter/scripts/shBrushSql
+//= require third-party/syntaxhighlighter/scripts/shBrushXml
+//= require third-party/syntaxhighlighter/scripts/shBrushScala
+
+
+SyntaxHighlighter.all();
